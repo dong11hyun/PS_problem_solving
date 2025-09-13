9d335d5391abe43085010d9f77b0d8560a007f83
dong11hyun <eastmanin@gmail.com>
2025-09-13 22:51:15 +0900

    dd

--- programmers/level_0/2025_09_14.py
+++ programmers/level_0/2025_09_14.py
@@ -1,3 +1,4 @@
+
 #특이한 정렬
 # 1. sorted() 함수를 사용하여 numlist를 정렬합니다.
     # 2. key 인자에 lambda 함수를 전달하여 커스텀 정렬 기준을 정의합니다.
@@ -6,5 +7,6 @@
     #      - 첫 번째 기준: n과의 거리(abs(x - n)). 이 값을 기준으로 오름차순 정렬.
     #      - 두 번째 기준: 원소의 음수값(-x). 거리가 같을 경우 이 값을 기준으로 오름차순 정렬
     #        (결과적으로 원래 값 x는 내림차순 정렬됨).
+    #      -sorted(함수)
 def solution(numlist, n):
     return sorted(numlist, key=lambda x: (abs(x-n), -x))
