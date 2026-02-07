11d07700580a17c3821891c5082bc2db4e45c9ca
dong11hyun <eastmanin@gmail.com>
2026-02-07 22:05:28 +0900

    .

--- baekjoon/19_재귀/B2_재귀의귀재.py
+++ baekjoon/19_재귀/B2_재귀의귀재.py
@@ -1,18 +1,14 @@
-
-def recursion(s, l, r, count):
-    count += 1
-    if l >= r: 
+def recursion(s,l,r,count):
+    if l >= r:
         return 1, count
     elif s[l] != s[r]:
         return 0, count
     else:
-        return recursion(s, l+1, r-1, count)
-
-def isPalindrome(s):
-    return recursion(s, 0, len(s)-1, 0)
+        return recursion(s,l+1,r-1,count+1)
+def isPalin(s):
+    return recursion(s,0,len(s)-1,1)
 
 n = int(input())
 for i in range(n):
-    word = input()
-    result, count = isPalindrome(word)
+    result, count = isPalin(input())
     print(result, count)
--- baekjoon/19_재귀/B2_피보나치수5.py
+++ baekjoon/19_재귀/B2_피보나치수5.py
@@ -1,10 +1,19 @@
-n = int(input())
+# n = int(input())
+# def pibo(n):
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else :
+#         return pibo(n-1) + pibo(n-2)
+
+# print(pibo(n))
+
 def pibo(n):
     if n == 0:
         return 0
-    elif n == 1:
+    if n == 1:
         return 1
-    else :
-        return pibo(n-1) + pibo(n-2)
+    
+    return pibo(n-1) + pibo(n-2)
 
-print(pibo(n))
+print(pibo(int(input())))
